755ed24952d48a8bcd0522662fa501f8e47884de
Tinsae <tinsaeyohannes@gmail.com>
2024-08-16 23:44:10 +0300

    🚀feat(payment): Added accept payment route
    - Added a new route called `/acceptPayment`
    - This route uses the `acceptPayments` controller to handle incoming payment requests.

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,5 +1,8 @@
+import { acceptPayments } from '../controllers/payments.controller';
 import { Router } from 'express';
 
 const PaymentRouter = Router();
+
+PaymentRouter.post('/acceptPayment', acceptPayments);
 
 export default PaymentRouter;
