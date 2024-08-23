3b51e0d74b9f46f979f0efb2eb17e5db3f597f33
Tinsae <tinsaeyohannes@gmail.com>
2024-08-23 23:13:42 +0300

    🚀feat(payment) Added payment verification endpoint
    - Added a new endpoint `/verify/:id` to the PaymentRouter.
    - Added a new `verifyPayments` controller function to handle the verification request.
    - Modified the `acceptPayments` endpoint to be a POST request.
    - Updated the `PaymentRouter` to include both the `acceptPayments` and `verifyPayments` endpoints.

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,8 +1,11 @@
-import { acceptPayments } from '../controllers/payments.controller';
+import {
+  acceptPayments,
+  verifyPayments,
+} from '../controllers/payments.controller';
 import { Router } from 'express';
 
 const PaymentRouter = Router();
 
-PaymentRouter.post('/pay', acceptPayments);
+PaymentRouter.post('/pay', acceptPayments).post('/verify:id', verifyPayments);
 
 export default PaymentRouter;
